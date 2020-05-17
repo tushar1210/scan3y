e4fbc1bd62ea5e0d4ccd4aac0017f0e6575fa401
tushar.1210 <ts26192271@gmail.com>
2020-05-18 01:22:16 +0530

    Create daemon.py

--- scan3y.py
+++ scan3y.py
@@ -6,7 +6,7 @@ import re
 from datetime import datetime
 import shlex
 import logging
-
+from daemon import daemon
 
 def check(ports,poison):
     for p in ports:
@@ -21,7 +21,7 @@ def scan():
     remoteServer = "localhost"#str(input("Enter Remote Server : "))
     remoteServerIP = socket.gethostbyname(remoteServer)
     # print("-" * 60)
-    # print("Please wait, scanning remote host", remoteServerIP)
+    f.write("Scanning remote host  {} \n".format(remoteServerIP))
     # print("-" * 60)
     t1 = datetime.now()
     try:
@@ -52,32 +52,33 @@ def scan():
 f=open('.log','a')
 
 def main():
-    f.write('-'*60 + '\n' +' '*25 + 'New Session'+'\n')
-    subprocess.call('clear',shell=True)
-    ports = scan()
-    # poison = str(input('Enter the port '))
-    if ports==[]:
-        f.write('No open ports found \n')
-    for poison in ports:
-        cmd = "lsof -i :{}".format(poison)
-        args = shlex.split(cmd)
-        output,error = subprocess.Popen(args,stdout = subprocess.PIPE, stderr= subprocess.PIPE).communicate()
-        output=str(output)
-        pid = ''
-        for i in range(len(output)):
-            if output[i]>='0' and output[i]<='9':
-                while output[i]>='0' and output[i]<='9':
-                    pid+=output[i]
-                    i+=1
-                break
-        
-        f.write('\n')
-        cmd = "kill -9 {}".format(pid)
-        f.write('PID found : {} \n'.format(pid))
-        f.write('\n')
-        args = shlex.split(cmd)
-        output,error = subprocess.Popen(args,stdout = subprocess.PIPE, stderr=subprocess.PIPE).communicate()
-        if(error):
-            f.write('Error found : {}'.format(str(error)))
+    while(True):
+        f.write('-'*60 + '\n' +' '*25 + 'New Session'+'\n')
+        subprocess.call('clear',shell=True)
+        ports = scan()
+        # poison = str(input('Enter the port '))
+        if ports==[]:
+            f.write('No open ports found \n')
+        for poison in ports:
+            cmd = "lsof -i :{}".format(poison)
+            args = shlex.split(cmd)
+            output,error = subprocess.Popen(args,stdout = subprocess.PIPE, stderr= subprocess.PIPE).communicate()
+            output=str(output)
+            pid = ''
+            for i in range(len(output)):
+                if output[i]>='0' and output[i]<='9':
+                    while output[i]>='0' and output[i]<='9':
+                        pid+=output[i]
+                        i+=1
+                    break
+            
+            f.write('\n')
+            cmd = "kill -9 {}".format(pid)
+            f.write('PID found : {} \n'.format(pid))
+            f.write('\n')
+            args = shlex.split(cmd)
+            output,error = subprocess.Popen(args,stdout = subprocess.PIPE, stderr=subprocess.PIPE).communicate()
+            if(error):
+                f.write('Error found : {}'.format(str(error)))
 
 if __name__ == "__main__": main()
